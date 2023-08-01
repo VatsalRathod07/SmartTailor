573762bef766810b0046566a2d10a3f8feb4d0fb
VatsalRathod07 <vatsalrathod2005@gmail.com>
2023-08-01 15:28:10 +0530

    added an favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,6 +18,9 @@ export default function RootLayout({
 
   return (
     <html lang="en">
+      <head>
+        <link rel="shortcut icon" href="/img/favicon.svg" type="image/x-icon" />
+      </head>
       <body>
         <Announcement />
         <Navbar />
